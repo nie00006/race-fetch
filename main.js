f885f0f0e295fdccd9269093eb361f52580ef1fc
Jinsha Nie <43067717+nie00006@users.noreply.github.com>
2019-11-17 17:01:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let datafile1 = fetch("https://jsonbox.io/box_82b5a4aa7a4a71abaa80");
-let datafile2 = fetch("https://jsonbox.io/box_7ebd8cb0b8fb187873ef");
+let datafile1 = fetch("https://gist.githubusercontent.com/prof3ssorSt3v3/22eb7745efdee9e130ed13cc19d85b75/raw/9dd9becedcf05e4392323645f7d8618ff26b1c38/box_7ebd8cb0b8fb187873ef.json");
+let datafile2 = fetch("https://gist.githubusercontent.com/prof3ssorSt3v3/9fcda9dc6d5e94e5689de33b0d9d45f8/raw/f1a89ab08aff538c1a7d4b85d0d7ba76ea92b88e/box_82b5a4aa7a4a71abaa80.json");
 
 
 
